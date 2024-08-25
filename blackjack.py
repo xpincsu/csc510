85868d4a2280d87d4f621e7d51715d932194daf0
xpincsu <xpi@ncsu.edu>
2024-08-25 13:31:57 -0400

    Delete blackjack.py

--- blackjack.py
+++
@@ -1,9 +0,0 @@
-def blackjack(a,b,c):
-  if sum ([a,b,c]) <=21:
-    return sum([a,b,c])  
-  elif 11 in [a,b,c] and sum([a,b,c]) <= 21:
-    return  sum([a,b,c]) -10
-  else:
-    return 'BUST'
-
-print(blackjack(a:5,b:6c:7))
