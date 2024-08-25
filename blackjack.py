d0f49c515317d905f04db2649fa102652b246011
xpincsu <xpi@ncsu.edu>
2024-08-25 13:18:05 -0400

    Create blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,9 @@
+def blackjack(a,b,c):
+  if sum ([a,b,c]) <=21:
+    return sum([a,b,c])  
+  elif 11 in [a,b,c] and sum([a,b,c]) <= 21:
+    return  sum([a,b,c]) -10
+  else:
+    return 'BUST'
+
+print(blackjack(a:5,b:6c:7))
